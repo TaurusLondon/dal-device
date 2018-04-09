5dc4e7aecb676dd423380f62c8a32b73c778d97e
mahima.aggarwal <mahima.aggarwal@E13-RDS12-Microservice.newvoe.local>
2018-04-09 08:46:46 +0000

    Remove @JsonIgnore

--- dal-device-svc/src/main/java/com/vf/uk/dal/device/datamodel/product/Group.java
+++ dal-device-svc/src/main/java/com/vf/uk/dal/device/datamodel/product/Group.java
@@ -2,15 +2,13 @@ package com.vf.uk.dal.device.datamodel.product;
 
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 
 public class Group {
 
 	protected String groupName;
 
 	protected Long priority;
-	@JsonIgnore
+
 	protected boolean comparable;
 
 	protected List<Specification> specifications;
