1a303f0f5d988147d5b0c8c108ebcf8899eee25c
manoj <manoj@E13-RDS26-Micro.newvoe.local>
2018-07-20 13:54:07 +0000

    Lombok implemented

--- dal-device-svc/src/main/java/com/vf/uk/dal/utility/solr/entity/HardwarePrice.java
+++ dal-device-svc/src/main/java/com/vf/uk/dal/utility/solr/entity/HardwarePrice.java
@@ -2,11 +2,14 @@ package com.vf.uk.dal.utility.solr.entity;
 
 import java.util.List;
 
+import lombok.Data;
+
 /**
  * 
  * HardwarePrice
  *
  */
+@Data
 public class HardwarePrice {
 	private String hardwareId;
 
@@ -15,69 +18,4 @@ public class HardwarePrice {
 	private OneOffDiscountPrice oneOffDiscountPrice;
 
 	private List<DeviceFinancingOption> financingOptions = null;
-
-	/**
-	 * 
-	 * @return
-	 */
-	public List<DeviceFinancingOption> getFinancingOptions() {
-		return financingOptions;
-	}
-
-	/**
-	 * 
-	 * @param financingOptions
-	 */
-	public void setFinancingOptions(List<DeviceFinancingOption> financingOptions) {
-		this.financingOptions = financingOptions;
-	}
-
-	/**
-	 * 
-	 * @param hardwareId
-	 */
-	public void setHardwareId(String hardwareId) {
-		this.hardwareId = hardwareId;
-	}
-
-	/**
-	 * 
-	 * 
-	 * @return
-	 */
-	public String getHardwareId() {
-		return this.hardwareId;
-	}
-
-	/**
-	 * 
-	 * @param oneOffPrice
-	 */
-	public void setOneOffPrice(OneOffPrice oneOffPrice) {
-		this.oneOffPrice = oneOffPrice;
-	}
-
-	/**
-	 * 
-	 * @return
-	 */
-	public OneOffPrice getOneOffPrice() {
-		return this.oneOffPrice;
-	}
-
-	/**
-	 * 
-	 * @param oneOffDiscountPrice
-	 */
-	public void setOneOffDiscountPrice(OneOffDiscountPrice oneOffDiscountPrice) {
-		this.oneOffDiscountPrice = oneOffDiscountPrice;
-	}
-
-	/**
-	 * 
-	 * @return
-	 */
-	public OneOffDiscountPrice getOneOffDiscountPrice() {
-		return this.oneOffDiscountPrice;
-	}
 }
