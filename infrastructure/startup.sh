a23317daf11121d6bd74e627bc843eb90e825ebd
Ruaridh Angus <ruaridh.angus1@vodafone.com>
2018-09-03 11:24:57 +0000

    Updated startup.sh

--- infrastructure/startup.sh
+++ infrastructure/startup.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 if [[ -n "${java_opts}" ]]; then 
-    java_opts="-javaagent:/apps/appdynamics/appdagent/javaagent.jar -Dappdynamics.controller.hostName=${hostname} -Dappdynamics.controller.port=${appdport} -Dappdynamics.controller.ssl.enabled=true -Dappdynamics.agent.applicationName=DigitalX-pat -Dappdynamics.agent.tierName=${MSNAME} -Dappdynamics.agent.nodeName=${MSNAME} -Dappdynamics.agent.reuse.nodeName=true -Dappdynamics.agent.reuse.nodeName.prefix=${MSNAME} -Dappdynamics.jvm.shut
-    down.mark.node.as.historical=true -Dappdynamics.agent.accountAccessKey=${appdkey}"
+    java_opts="-javaagent:/apps/appdynamics/appdagent/javaagent.jar -Dappdynamics.controller.hostName=${hostname} -Dappdynamics.controller.port=${appdport} -Dappdynamics.controller.ssl.enabled=true -Dappdynamics.agent.applicationName=DigitalX-pat -Dappdynamics.agent.tierName=${MSNAME} -Dappdynamics.agent.nodeName=${MSNAME} -Dappdynamics.agent.reuse.nodeName=true -Dappdynamics.agent.reuse.nodeName.prefix=${MSNAME} -Dappdynamics.jvm.shutdown.mark.node.as.historical=true -Dappdynamics.agent.accountAccessKey=${appdkey}"
 fi
 
 java ${java_opts} -jar /apps/sb/${MSNAME}-svc-${VERSION}.jar
