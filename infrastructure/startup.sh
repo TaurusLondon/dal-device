7ce0790340e75b18547e6a36c3bb6f6d9fecece9
Ruaridh Angus <ruaridh.angus1@vodafone.com>
2018-09-04 14:33:08 +0000

    Updated startup.sh

--- infrastructure/startup.sh
+++ infrastructure/startup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-if [[ -n "${java_opts}" ]]; then 
-    java_opts="-javaagent:/apps/appdynamics/appdagent/javaagent.jar -Dappdynamics.controller.hostName=${hostname} -Dappdynamics.controller.port=${appdport} -Dappdynamics.controller.ssl.enabled=true -Dappdynamics.agent.applicationName=DigitalX-pat -Dappdynamics.agent.tierName=${MSNAME} -Dappdynamics.agent.nodeName=${MSNAME} -Dappdynamics.agent.reuse.nodeName=true -Dappdynamics.agent.reuse.nodeName.prefix=${MSNAME} -Dappdynamics.jvm.shutdown.mark.node.as.historical=true -Dappdynamics.agent.accountAccessKey=${appdkey}"
+if [[ "${APPD}" == "true" ]]; then 
+    JAVA_OPTS="-javaagent:/apps/appdynamics/appdagent/javaagent.jar -Dappdynamics.controller.hostName=${hostname} -Dappdynamics.controller.port=${appdport} -Dappdynamics.controller.ssl.enabled=true -Dappdynamics.agent.applicationName=DigitalX-pat -Dappdynamics.agent.tierName=${MSNAME} -Dappdynamics.agent.nodeName=${MSNAME} -Dappdynamics.agent.reuse.nodeName=true -Dappdynamics.agent.reuse.nodeName.prefix=${MSNAME} -Dappdynamics.jvm.shutdown.mark.node.as.historical=true -Dappdynamics.agent.accountAccessKey=${appdkey} -Xmx${XMX} -Xms${XMS}"
+else
+    JAVA_OPTS="-Xmx${XMX} -Xms${XMS}"
 fi
 
-java -javaagent:/apps/appdynamics/appdagent/javaagent.jar -Dappdynamics.controller.hostName=${hostname} -Dappdynamics.controller.port=${appdport} -Dappdynamics.controller.ssl.enabled=true -Dappdynamics.agent.applicationName=DigitalX-pat -Dappdynamics.agent.tierName=${MSNAME} -Dappdynamics.agent.nodeName=${MSNAME} -Dappdynamics.agent.reuse.nodeName=true -Dappdynamics.agent.reuse.nodeName.prefix=${MSNAME} -Dappdynamics.jvm.shutdown.mark.node.as.historical=true -Dappdynamics.agent.accountAccessKey=${appdkey} -Xmx384m -Xms384m -jar /apps/sb/${MSNAME}-svc-${VERSION}.jar
+java $JAVA_OPTS -jar /apps/sb/${MSNAME}-svc-${VERSION}.jar
