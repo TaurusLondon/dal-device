3e4a6765e8a37d8c17bf49f0413e0853353407f7
manoj <manoj@E13-RDS26-Micro.newvoe.local>
2018-09-04 06:43:37 +0000

    Bug 45345 fixed

--- src/main/java/com/vf/uk/dal/device/datamodel/productgroups/ProductGroupModel.java
+++ src/main/java/com/vf/uk/dal/device/datamodel/productgroups/ProductGroupModel.java
@@ -29,52 +29,76 @@ public class ProductGroupModel {
 
 	@JsonProperty("minimumRecurringPrice")
 	private float minimumRecurringPrice;
+	
 	@JsonProperty("minimumUpfrontCost")
 	private float minimumUpfrontCost;
+	
 	@JsonProperty("rating")
 	private float rating;
+	
 	@JsonProperty("listOfVariants")
 	private List<String> listOfVariants;
+	
 	@JsonProperty("priority")
 	private int priority;
+	
 	@JsonProperty("type")
 	private String type;
+	
 	@JsonProperty("leadPlanId")
 	private String leadPlanId;
+	
 	@JsonProperty("equipmentMake")
 	private String equipmentMake;
+	
 	@JsonProperty("equipmentModel")
 	private String equipmentModel;
+	
 	@JsonProperty("colour")
 	private List<String> colour;
+	
 	@JsonProperty("capacity")
 	private List<String> capacity;
+	
 	@JsonProperty("operatingSystem")
 	private List<String> operatingSystem;
+	
 	@JsonProperty("leadStartDate")
 	private String leadStartDate;
+	
 	@JsonProperty("mustHaveFeatures")
 	private List<String> mustHaveFeatures;
+	
 	@JsonProperty("leadDeviceId")
 	private String leadDeviceId;
+	
 	@JsonProperty("minimumCost")
 	private Float minimumCost;
+	
 	@JsonProperty("isLeadMember")
 	private String isLeadMember;
+	
 	@JsonProperty("facetColour")
 	private List<String> facetColour;
+	
 	@JsonProperty("upgradeLeadDeviceId")
 	private String upgradeLeadDeviceId;
+	
 	@JsonProperty("nonUpgradeLeadDeviceId")
 	private String nonUpgradeLeadDeviceId;
+	
 	@JsonProperty("upgradeLeadPlanId")
 	private String upgradeLeadPlanId;
+	
 	@JsonProperty("nonUpgradeLeadPlanId")
 	private String nonUpgradeLeadPlanId;
+	
 	@JsonProperty("createDate")
 	private Date createDate;
+	
 	@JsonProperty("modifiedDate")
 	private Date modifiedDate;
+	
 	@JsonProperty("hexCode")
 	private List<String> hexCode;
 
