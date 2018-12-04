afbd7d723bdd06cb13264c2eb682bcb82f402a6a
sahil.monga <sahil.monga@E13-RDS26-Micro.newvoe.local>
2018-12-04 10:50:58 +0000

    Error class for integration test

--- src/main/java/com/vf/uk/dal/device/model/ErrorPopulation.java
+++ src/main/java/com/vf/uk/dal/device/model/ErrorPopulation.java
@@ -5,16 +5,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 /**
- * Error
+ * Error population for integration test
  */
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2018-01-02T12:25:09.565Z")
 @Data
-public class Error {
-	@JsonProperty("code")
+public class ErrorPopulation {
+	@JsonProperty("errorCode")
 	private String code = null;
 
-	@JsonProperty("message")
+	@JsonProperty("errorMessage")
 	private String message = null;
 
 	@JsonProperty("referenceId")
