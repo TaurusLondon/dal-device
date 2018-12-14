54be65fd7011dbd0a487bb0e47b87466be916bac
sahil.monga <sahil.monga@E13-RDS26-Micro.newvoe.local>
2018-12-14 10:31:39 +0000

    test cases update

--- src/test/java/com/vf/uk/dal/device/common/test/CommonMethods.java
+++ src/test/java/com/vf/uk/dal/device/common/test/CommonMethods.java
@@ -607,46 +607,60 @@ public class CommonMethods {
 		deviceList.setPriceInfo(getPriceForBundleAndHardware().get(0));
 
 		Device deviceList1 = new Device();
-		deviceList.setDeviceId("092660");
-		deviceList.setMake("Apple");
-		deviceList.setModel("iphone7");
-		deviceList.setGroupType("DEVICE_PAYM");
-		deviceList.setRating("4");
-		deviceList.setDescription("Description");
-		deviceList.setProductClass("HANDSET");
-		deviceList.setMerchandisingControl(merchandisingControl);
-		deviceList.setMedia(merchandisingMedia);
-		deviceList.setPriceInfo(getPriceForBundleAndHardware().get(0));
+		deviceList1.setDeviceId("092660");
+		deviceList1.setMake("Apple");
+		deviceList1.setModel("iphone7");
+		deviceList1.setGroupType("DEVICE_PAYM");
+		deviceList1.setRating("4");
+		deviceList1.setDescription("Description");
+		deviceList1.setProductClass("HANDSET");
+		deviceList1.setMerchandisingControl(merchandisingControl);
+		deviceList1.setMedia(merchandisingMedia);
+		deviceList1.setPriceInfo(getPriceForBundleAndHardware().get(0));
 
 		Device deviceList2 = new Device();
-		deviceList.setDeviceId("094127");
-		deviceList.setMake("Apple");
-		deviceList.setModel("iphone7");
-		deviceList.setGroupType("DEVICE_PAYM");
-		deviceList.setRating("4");
-		deviceList.setDescription("Description");
-		deviceList.setProductClass("HANDSET");
-		deviceList.setMerchandisingControl(merchandisingControl);
-		deviceList.setMedia(merchandisingMedia);
-		deviceList.setPriceInfo(getPriceForBundleAndHardware().get(0));
+		deviceList2.setDeviceId("094127");
+		deviceList2.setMake("Apple");
+		deviceList2.setModel("iphone7");
+		deviceList2.setGroupType("DEVICE_PAYM");
+		deviceList2.setRating("4");
+		deviceList2.setDescription("Description");
+		deviceList2.setProductClass("HANDSET");
+		deviceList2.setMerchandisingControl(merchandisingControl);
+		deviceList2.setMedia(merchandisingMedia);
+		deviceList2.setPriceInfo(getPriceForBundleAndHardware().get(0));
 
 		Device deviceList3 = new Device();
-		deviceList.setDeviceId("01234");
-		deviceList.setMake("Apple");
-		deviceList.setModel("iphone7");
-		deviceList.setGroupType("DEVICE_PAYM");
-		deviceList.setRating("4");
-		deviceList.setDescription("Description");
-		deviceList.setProductClass("HANDSET");
-		deviceList.setMerchandisingControl(merchandisingControl);
-		deviceList.setMedia(merchandisingMedia);
-		deviceList.setPriceInfo(getPriceForBundleAndHardware().get(0));
+		deviceList3.setDeviceId("01234");
+		deviceList3.setMake("Apple");
+		deviceList3.setModel("iphone7");
+		deviceList3.setGroupType("DEVICE_PAYM");
+		deviceList3.setRating("4");
+		deviceList3.setDescription("Description");
+		deviceList3.setProductClass("HANDSET");
+		deviceList3.setMerchandisingControl(merchandisingControl);
+		deviceList3.setMedia(merchandisingMedia);
+		deviceList3.setPriceInfo(getPriceForBundleAndHardware().get(0));
+		deviceList3.setPromotionsPackage(getMerchandisingPromotionsPackage());
 		deviceLists.add(deviceList);
 		deviceLists.add(deviceList1);
 		deviceLists.add(deviceList2);
 		deviceLists.add(deviceList3);
 
 		return deviceLists;
+	}
+
+	private static MerchandisingPromotionsPackage getMerchandisingPromotionsPackage() {
+		MerchandisingPromotionsPackage merchandisingPromotionsPackage = new MerchandisingPromotionsPackage();
+		merchandisingPromotionsPackage.setBundlePromotions(getMerchandisingPromotionsWrapper());
+		merchandisingPromotionsPackage.setHardwarePromotions(getMerchandisingPromotionsWrapper());
+		return merchandisingPromotionsPackage;
+	}
+
+	private static MerchandisingPromotionsWrapper getMerchandisingPromotionsWrapper() {
+		MerchandisingPromotionsWrapper merchandisingPromotionsWrapper = new MerchandisingPromotionsWrapper();
+		merchandisingPromotionsWrapper.setPricePromotion(getMP());
+		return merchandisingPromotionsWrapper;
 	}
 
 	public static List<DeviceTile> getDeviceTile(String make, String model, String groupType) {
@@ -2903,6 +2917,8 @@ public class CommonMethods {
 		merchandisingPromotions.setPriority(Long.valueOf(1));
 		merchandisingPromotions.setLabel("Label");
 		merchandisingPromotions.setVersion("Version");
+		merchandisingPromotions.setCondition(getCondition());
+		merchandisingPromotions.setFootNoteKey("footNote");
 		merchandisingPromotionsList.add(merchandisingPromotions);
 		return merchandisingPromotionsList;
 	}
