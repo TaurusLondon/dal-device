19e1794e4c68213d150ec36cc2c912d711136c3f
Ruaridh Angus <ruaridh.angus1@vodafone.com>
2019-01-14 14:13:05 +0000

    re-creating .sh file

--- infrastructure/startup.sh
+++ infrastructure/startup.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 if [[ "${AppDFlag}" == "true" ]]; then 
     JAVA_OPTS="-javaagent:/apps/appdynamics/appdagent/javaagent.jar -Dappdynamics.controller.hostName=${Hostname} -Dappdynamics.controller.port=${AppDPort} -Dappdynamics.controller.ssl.enabled=true -Dappdynamics.agent.applicationName=DigitalX-${Environment} -Dappdynamics.agent.tierName=${MSNAME} -Dappdynamics.agent.nodeName=${MSNAME} -Dappdynamics.agent.reuse.nodeName=true -Dappdynamics.agent.reuse.nodeName.prefix=${MSNAME} -Dappdynamics.jvm.shutdown.mark.node.as.historical=true -Dappdynamics.agent.accountAccessKey=${AppDKey} -Xmx${XMX} -Xms${XMS}"
 else
