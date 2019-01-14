1cc05195b7ca5f8d1bba4f774bc204fab32bdb81
RuaridhAngus <ruaridh.angus1@vodafone.com>
2019-01-14 11:33:17 +0000

    Updated startup.sh

--- infrastructure/startup.sh
+++ infrastructure/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [[ "${APPD}" == "true" ]]; then 
-    JAVA_OPTS="-javaagent:/apps/appdynamics/appdagent/javaagent.jar -Dappdynamics.controller.hostName=${hostname} -Dappdynamics.controller.port=${appdport} -Dappdynamics.controller.ssl.enabled=true -Dappdynamics.agent.applicationName=DigitalX-${Environment} -Dappdynamics.agent.tierName=${MSNAME} -Dappdynamics.agent.nodeName=${MSNAME} -Dappdynamics.agent.reuse.nodeName=true -Dappdynamics.agent.reuse.nodeName.prefix=${MSNAME} -Dappdynamics.jvm.shutdown.mark.node.as.historical=true -Dappdynamics.agent.accountAccessKey=${appdkey} -Xmx${XMX} -Xms${XMS}"
+if [[ "${AppDFlag}" == "true" ]]; then 
+    JAVA_OPTS="-javaagent:/apps/appdynamics/appdagent/javaagent.jar -Dappdynamics.controller.hostName=${Hostname} -Dappdynamics.controller.port=${AppDPort} -Dappdynamics.controller.ssl.enabled=true -Dappdynamics.agent.applicationName=DigitalX-${Environment} -Dappdynamics.agent.tierName=${MSNAME} -Dappdynamics.agent.nodeName=${MSNAME} -Dappdynamics.agent.reuse.nodeName=true -Dappdynamics.agent.reuse.nodeName.prefix=${MSNAME} -Dappdynamics.jvm.shutdown.mark.node.as.historical=true -Dappdynamics.agent.accountAccessKey=${AppDKey} -Xmx${XMX} -Xms${XMS}"
 else
     JAVA_OPTS="-Xmx${XMX} -Xms${XMS}"
 fi
